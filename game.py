7b989b01b65f0dc6680196589b84fcc205081aeb
Leon <leonp92@vt.edu>
2013-04-15 09:33:41 -0400

    Basic files commit

---
+++ game.py
@@ -0,0 +1,6 @@
+import sys
+
+def userMove(userChoice):
+	print("Test input")
+	userChoice=raw_input("Choose your option: \n")
+	print(userChoice)
---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+
+import game, errors
+
+def main():
+	userChoice = ""
+	game.userMove(userChoice)
+
+if __name__ == "__main__":
+	main()
