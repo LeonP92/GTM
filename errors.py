960f283cba20aae91ae7862fffb40211a12ba9e9
Leon <leonp92@vt.edu>
2013-04-15 09:43:02 -0400

    Add error checking to user choice

---
+++ errors.py
@@ -0,0 +1,4 @@
+import sys
+
+def badChoice():
+	print("Error: The choice you chose is not a possible choice")
--- game.py
+++ game.py
@@ -1,6 +1,19 @@
-import sys
+import sys, errors
+
+possibleChoice = ('A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e')
 
 def userMove(userChoice):
+	possible = 0
 	print("Test input")
 	userChoice=raw_input("Choose your option: \n")
-	print(userChoice)
+	
+	for choice in possibleChoice:
+		if possible == 0: 
+			if choice == userChoice:
+				possible = 1;
+			else: #do nothing
+				break
+	if possible == 1:
+		print(userChoice)
+	else:
+		errors.badChoice()
