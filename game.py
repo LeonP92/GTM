9d3dc89bba61eed850271f5d35f3a79be8a14027
Grant Spence <gcs278@vt.edu>
2013-04-19 23:09:47 -0400

    Fighting almost done

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
-import sys, errors, time, os, fileinput, re, random,string
+import sys, errors, time, os, fileinput, re, random, string
 
 possibleChoice = ('A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'Q', 'q') #used for basic choices
-items = ('baseball bat', 'baseball', 'bat', 'knife', 'bow', 'pistol', 'rifle', 'steal baseball bat', 'steal baseball', 'steal bat', 'steal knife', 'steal bow', 'steal pistol', 'steal rifle') #Used for shop info
+items = ('bat', 'knife', 'bow', 'pistol', 'rifle') #Used for shop info
 fighters = ('Louie','Bob','Darren','Big Boy Bruno','Tyrone','Pedro','Hilter','Martin','Hobo Martin','Fat Joe',\
 'Oscar','Alfonso','Swollen Lou','Butter Knife Pietro','Busted Kneecaps Fabrizio','Petty Crime Salvatore') #Fighter's names
 
@@ -94,7 +94,8 @@ def intro(file):
 	#Writes in character file
 	file.write("Name: "+ username +"\nLevel: 1\nHealth: 100\nMoney: 0\nItems: none\nMission: 0")	
 #Description this will be the basic user move, when they are not in the missions or fights
-def usermove(optionList):
+# *** Input numbers=0 uses ABC numbers=1 uses 1,2,3
+def usermove(optionList,numbers):
 	possible = 0
 	i = 0 #temporary variable
 	for items in optionList:
@@ -108,7 +109,10 @@ def usermove(optionList):
 		else:
 			break
 	if possible == 1:
-		userChoice = userChoice.lower()
+		if numbers == 0:
+			userChoice = userChoice.lower()
+		elif numbers == 1:
+			userChoice = ord(userChoice.lower()) - ord('a')+1
 		return userChoice
 	else:
 		errors.badChoice()
@@ -141,6 +145,7 @@ def showshop(toShow):
 		
 #Description: Function for the fight scene
 def fight(environment):
+	os.system("clear")
 	# Determine fighter
 	if environment == "store":
 		fighter = "Clerk"
@@ -153,66 +158,120 @@ def fight(environment):
 	fighterHealth = int(parse("Level"))*random.randrange(60,120)
 	print(fighter+" has appeared and wants to throw some punches!")
 	print(fighter+"'s health is "+str(fighterHealth))
-	
+	time.sleep(2)
+	os.system("clear")
+	damage = 0
 	# Fight and move selection
 	myHealth = int(parse("Health"))
-	while myHealth>0 and fighterHealth>0:
-		print("What do you want to do?")
-		# Your Move
-		userMove = usermove(["A) Use Item","B) Punch","C) Kick","D) Block"])
-		successHit = 0
-		if userMove=='a':
-			print("Choose your item")
-			itemUsed = usermove(getitems())
-			print(itemUsed)
-		elif userMove=='b':
-			print("You threw a punch at "+fighter)
-			damage = attack("me")
-		elif userMove=='c':
-			print("You threw a kick at "+fighter)
-			damage = attack("me")
-		elif userMove=='d':
-			print("You blocked!")
 
+	while myHealth>0 and fighterHealth>0:
+		damage = userFightMove(fighter)
+		time.sleep(2)
+		os.system("clear")
 		if damage>0:
 			fighterHealth=fighterHealth - damage
 			if fighterHealth<=0:
 				break
-			print(fighter+"'s health is now "+str(fighterHealth))
+			print(hilight(fighter+"'s health is now "+str(fighterHealth),'34',1))
+		elif damage==-1:
+			break
 		time.sleep(2)
+		os.system("clear")
 		print(fighter+"'s Move..")
 		# Fighter's Move
-		damage = attack("Fighter")
+		damage = attack("Fighter",5)
 		if damage>0:
 			myHealth=myHealth-damage
 			if myHealth<=0:
 				break
-			print("Your health is now "+str(myHealth))
-		time.sleep(2)
+			elif myHealth>=80:
+				print("Your health is now "+hilight(str(myHealth),'32',1))
+			elif myHealth<80 and myHealth>=30:	
+				print("Your health is now "+hilight(str(myHealth),'33',1))
+			elif myHealth<30:
+				print("Your health is now "+hilight(str(myHealth),'31',1))
+		time.sleep(4)
+		os.system("clear")
+	
 	if fighterHealth<=0:
-		print("You won!")
-	else:
-		print("You lost loser...")
+		print(hilight("You won!",'32',1))
+	elif myHealth<=0:
+		print(hilight("You lost loser...",'31',1))
 	time.sleep(5)
+#Description: Determines User move on thier input
+def userFightMove(fighter):
+	while(1):
+		print("What do you want to do?")
+		# Your Move
+		userMove = usermove(["A) Use Item","B) Punch","C) Kick","D) Run"],0)
+		os.system("clear")
+		if userMove=='a':
+			print("Choose your item")
+			userMove = usermove(getitems(),1)
+			os.system("clear")
+			damage = itemAttack(userMove)
+			if not damage == -1:
+				return damage
+		elif userMove=='b':
+			print("You threw a punch at "+fighter)
+			return attack("me",5)
+		elif userMove=='c':
+			print("You threw a kick at "+fighter)
+			return attack("me",5)
+		elif userMove=='d':
+			if random.randrange(0,2)==1:
+				print(hilight("WHEW! You got out of this joker's way",'32',1))
+				return -1
+			else:
+				print(hilight(fighter+" grabbed you so you wouldn't run!",'31',1))
+	return 0
 #Description: Determines the power of an attack
-def attack(whichFighter):
+# DamageRange is first value a decent hit will randomize at
+def attack(whichFighter,damageRange):
 	if whichFighter=="me":
-		myLevel = int(parse("Level"))
+		level = int(parse("Level"))
 	else:
-		myLevel = 0
-	for i in range(myLevel+1):
+		level = 0
+	for i in range(level+1):
 		hitType = random.randrange(0,3)
 		if hitType==0 or hitType==1:
 			break
+	damage = 0
 	if hitType==0:
-		print("Miss yo!")
+		print(hilight("Miss yo!",'31',1))
 		return 0
 	elif hitType==1:
-		print("Decent Hit!")
-		return random.randrange(10,25)
+		damage = random.randrange(damageRange,damageRange+10)
+		print(hilight("Decent Hit of "+str(damage),'33',1))
+		return damage
 	elif hitType==2:
-		print("Critical Hit!")
-		return random.randrange(26,50)
+		damage= random.randrange(damageRange+11,damageRange+20)
+		print(hilight("Critical Hit of "+str(damage),'32',1))
+		return damage
+#Description: Determines attack when using an item
+def itemAttack(item):
+	itemList = getitems()
+	if not item:
+		return -1
+	item = itemList[item-1]
+	item = item.replace(") ","")
+	item = item.replace("\n","")
+	item = item[1:]
+	
+	if item=="bat":
+		return attack("me",10)
+	elif item=="knife":
+		return attack("me",15)
+	elif item=="bow":
+		return attack("me",25)
+	elif item=="pistol":
+		return attack("me",35)
+	elif item=="rifle":
+		return attack("me",60)
+	else:
+		print("ERROR: FILE TAMPER")
+		return 0
+	
 #Description: Returns items user have, uses string to append letters for selection
 def getitems():
 	allTheLetters = string.uppercase
@@ -231,3 +290,17 @@ def parse(getItem):
 	for i, line in enumerate(file):
 		if re.match(getItem,line):
 			return line.replace(getItem+": ","")
+#Description: Changes the color of the string, USE COLOR CHART, bold=1
+def hilight(string, color, bold):
+	attr = []
+	attr.append(color)
+	if bold:
+		attr.append('1')
+	return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), string)
+
+#Description: Changes the value of the desire character attribute in the file
+def changeFileValue(attribute,value):
+	file = open("character.txt","r")
+	
+	#if attribute=="health":
+		# Replace the health variable
--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import game, errors, time
 def main():
 	userChoice = ""
 	gameOn = 0
+	game.fight("street")
 	gameOn = game.greetings()
 	while gameOn==0: 
-		userChoice = game.usermove(["A) Fight Stranger", "B) Rob Stranger", "C) Rest", "D) Shop", "E) Cry"])
+		userChoice = game.usermove(["A) Fight Stranger", "B) Rob Stranger", "C) Rest", "D) Shop", "E) Cry"],0)
 		if userChoice == 'd':
 			game.showshop(0)
 		if userChoice == 'q':
